9af341de94f9a48b461e0d787716c7bd486fc336
Jackson <jacksonmungai001@gmail.com>
2025-08-30 00:00:45 +0300

    new update

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,15 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+	def __init__(self, addresses):
+		self.addresses = addresses
+
+	def parse(self):
+		# Split by comma or whitespace
+		parts = re.split(r'[\s,]+', self.addresses.strip())
+		# Regex for a valid email address
+		email_regex = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+		# Filter only valid emails
+		emails = [email for email in parts if re.match(email_regex, email)]
+		# Get unique and sort
+		return sorted(set(emails))
